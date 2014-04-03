4162d6dc8ca9e911232d83a153daa91a7e85acb6
jkemper3 <jkemper3@L11702038596.wctc.msft>
2014-04-03 17:46:32 -0500

    Lab 1 Commit 1

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,48 @@
+
+
+package lab1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jkemper3
+ */
+public class Lab1 {
+    public static void main(String[] args) {
+        File data = new File("src/lab1.txt");              
+	  		
+	
+        BufferedReader in = null;
+        List<String> fileContents = new ArrayList<String>();
+       
+        try {
+	   in = new BufferedReader(new FileReader(data));
+	   String line = in.readLine();
+	   while(line != null){
+               fileContents.add(line);
+               if(fileContents.== 1){
+		  System.out.println(line);
+		  line = in.readLine();  // strips out any carriage return chars
+	   }
+           }
+        } catch(IOException ioe) {
+            System.out.println("Houston, we have a problem! reading this file");
+        } finally {
+            try {
+                in.close();
+            } catch(Exception e) {
+                
+            }
+        }
+        
+   
+    } 
+} 
+
+    
